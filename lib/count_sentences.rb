1c67322c5a62caa000874ad54d88efdf162e31ba
Loren Cipriano <loren25cipriano@gmail.com>
2020-11-28 16:54:37 -0800

    commit tree 99484326d1462c144669eda49e32a2e1086c90ab

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.?!]+/).count  # split the punctuation &  + "one or more" of something
   end
 end
+
+
+# String
+#   #sentence?
+#     returns true if the string that you are calling this method on ends in a period.
+#     returns false if the string that you are calling this method on does NOT end in a period.
